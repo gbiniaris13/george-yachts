a4781bb47872bad83c8a7f795608d7f28e3e54c4
Alexandros Kourmoulakis <akvrmvlakis@gmail.com>
2025-10-30 12:44:18 +0200

    add apollo.io script

--- app/layout.jsx
+++ app/layout.jsx
@@ -25,6 +25,23 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Apollo.io Website Tracker */}
+        <Script id="apollo-tracker" strategy="afterInteractive">
+          {`
+            function initApollo() {
+              var n = Math.random().toString(36).substring(7),
+                  o = document.createElement("script");
+              o.src = "https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache=" + n;
+              o.async = true;
+              o.defer = true;
+              o.onload = function() {
+                window.trackingFunctions.onLoad({ appId: "6900ba1d130409000d727aa4" });
+              };
+              document.head.appendChild(o);
+            }
+            initApollo();
+          `}
+        </Script>
         {recaptchaKey && (
           <Script
             id="recaptcha-script"
